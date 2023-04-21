5125e120a9e98ae5812df107c89977d5625ac831
Sung Lin Chua <darrell@Sungs-MacBook-Pro.local>
2023-04-22 04:27:53 +0800

    completed minishell

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -6,17 +6,15 @@
 /*   By: suchua <suchua@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 22:12:19 by suchua            #+#    #+#             */
-/*   Updated: 2023/04/20 19:36:30 by suchua           ###   ########.fr       */
+/*   Updated: 2023/04/22 03:57:22 by suchua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	**get_echo_strs(char *cmd)
+void	print_echo_str(char *cmd)
 {
-	int		i;
-	char	**s_cmd;
-	char	*tmp;
+	int	i;
 
 	i = 0;
 	while (cmd[i] && !ft_isspace(cmd[i]))
@@ -26,33 +24,19 @@ char	**get_echo_strs(char *cmd)
 	if (!ft_strncmp(&cmd[i], "-n", 2))
 		while (cmd[i] && !ft_isspace(cmd[i]))
 			++i;
-	tmp = ft_strdup("\"");
-	tmp = gnl_strioin(tmp, &cmd[i]);
-	tmp = gnl_strioin(tmp, "\"");
-	s_cmd = interesting_split(tmp, 0, 0, 0);
-	free(tmp);
-	return (s_cmd);
+	ft_putstr_fd(&cmd[i], 1);
 }
 
 void	ft_echo(char **split, char *cmd, t_shell *info)
 {
 	int		flag;
-	int		i;
-	char	**s_cmd;
 
 	flag = 0;
 	if (!ft_strncmp(split[1], "-n", 3))
 		flag = 1;
-	s_cmd = get_echo_strs(cmd);
-	i = -1;
-	while (s_cmd[++i])
-	{
-		ft_putstr_fd(s_cmd[i], 1);
-		ft_putchar_fd(32, 1);
-	}
+	print_echo_str(cmd);
 	if (flag)
 		ft_putstr_fd("%", 1);
 	ft_putchar_fd('\n', 1);
 	info->ms_status = 0;
-	ft_free2d(s_cmd);
 }
