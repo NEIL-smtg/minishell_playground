b6da89e617e767dc6d9c9f97962fdf1ffb765bb7
NEIL-smtg <sunghui2000@hotmail.com>
2023-05-15 16:10:28 +0800

    fixed minor bugs in parse_input

--- srcs/builtins/ft_builtin.c
+++ srcs/builtins/ft_builtin.c
@@ -6,11 +6,28 @@
 /*   By: suchua <suchua@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 22:03:14 by suchua            #+#    #+#             */
-/*   Updated: 2023/05/11 18:28:16 by suchua           ###   ########.fr       */
+/*   Updated: 2023/05/15 16:07:12 by suchua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	parent_exec_decider(char **s_cmd)
+{
+	int	flag;
+
+	flag = 0;
+	if (!ft_strncmp("cd", s_cmd[0], 3))
+		flag = 1;
+	else if (!ft_strncmp("export", s_cmd[0], 7))
+		flag = 1;
+	else if (!ft_strncmp("unset", s_cmd[0], 6))
+		flag = 1;
+	else if (!ft_strncmp("exit", s_cmd[0], 5))
+		flag = 1;
+	ft_free2d(s_cmd);
+	return (flag);
+}
 
 int	parent_exec(t_shell *info, t_cmdlst *node)
 {
@@ -19,8 +36,9 @@ int	parent_exec(t_shell *info, t_cmdlst *node)
 
 	if (node->next && !ft_strncmp(node->next->cmd, "|", 2))
 		return (0);
-	ft_parse_input(info, &node);
-	flag = 1;
+	flag = parent_exec_decider(ft_split(node->cmd, 32));
+	if (flag)
+		ft_parse_input(info, &node);
 	s_cmd = ft_split(node->cmd, 32);
 	if (!ft_strncmp("cd", s_cmd[0], 3))
 		ft_cd(s_cmd, info);
@@ -30,8 +48,6 @@ int	parent_exec(t_shell *info, t_cmdlst *node)
 		ft_unset(s_cmd, info);
 	else if (!ft_strncmp("exit", s_cmd[0], 5))
 		exit(EXIT_SUCCESS);
-	else
-		flag = 0;
 	ft_free2d(s_cmd);
 	return (flag);
 }
