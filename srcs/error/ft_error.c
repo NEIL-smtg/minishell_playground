b97d43a0d07aa0aa5ecf16db809f1e09c8397ab7
NEIL-smtg <sunghui2000@hotmail.com>
2023-09-23 06:01:12 +0800

    fixed cmd not found path

--- srcs/error/ft_error.c
+++ srcs/error/ft_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_error.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: suchua <suchua@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*   By: suchua <suchua@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 01:21:52 by suchua            #+#    #+#             */
-/*   Updated: 2023/05/16 16:52:19 by suchua           ###   ########.fr       */
+/*   Updated: 2023/09/23 06:00:27 by suchua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ void	cmd_not_found(t_cmdlst *node, t_shell *info)
 		}
 		splitted = ft_split(node->cmd, 32);
 		path = get_cmd_path(splitted[0], info->ms_env);
-		if (path && !ft_strncmp(path, splitted[0], ft_strlen(path)))
+		if ((path && !ft_strncmp(path, splitted[0], ft_strlen(path))) || !path)
 		{
 			ft_putstr_fd("minishell : command not found: ", 2);
 			ft_putendl_fd(splitted[0], 2);
